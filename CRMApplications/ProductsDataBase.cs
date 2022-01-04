2b2cb7ac7209817aef64163196e0314bd750968b
Olena98 <olenadidenko98@gmail.com>
2022-01-04 21:43:06 +0200

    Add new method AddChangeEntriesNode

--- CRMApplications/ProductsDataBase.cs
+++ CRMApplications/ProductsDataBase.cs
@@ -11,10 +11,10 @@ namespace CRMApplications
 {
     class ProductsDataBase
     {
-        private static string xmlPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "productWF.xml"); 
-        
+        private static string xmlPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "productWF.xml");
+
         public static void Initialize()
-        {          
+        {
             ProductService.Products = ReadXmlFile(xmlPath);
         }
 
@@ -24,16 +24,16 @@ namespace CRMApplications
             {
                 return;
             }
-            XmlDocument xDoc = new XmlDocument();           
+            XmlDocument xDoc = new XmlDocument();
             XmlNode rootElement = xDoc.CreateNode(XmlNodeType.Element, "products", string.Empty);
-            xDoc.AppendChild(rootElement);           
-                           
-                foreach (Product product in ProductService.Products)
-                {
-                    AppendProductNode(rootElement, product);
-                }
+            xDoc.AppendChild(rootElement);
 
-                xDoc.Save(xmlPath);                     
+            foreach (Product product in ProductService.Products)
+            {
+                AppendProductNode(rootElement, product);
+            }
+
+            xDoc.Save(xmlPath);
         }
         private static void AppendProductNode(XmlNode parentNode, Product product)
         {
@@ -45,10 +45,11 @@ namespace CRMApplications
             productElem.SetAttribute("productNumber", product.ProductNumber.ToString());
             productElem.SetAttribute("existence", product.Existence.ToString());
 
+            AddChangeEntriesNode(productElem, product.ChangeEntries);
             parentNode.AppendChild(productElem);
         }
         public static List<Product> ReadXmlFile(string xmlPath)
-        {         
+        {
             List<Product> products = new List<Product>();
             var doc = new XmlDocument();
             if (File.Exists(xmlPath))
@@ -61,7 +62,7 @@ namespace CRMApplications
                 doc.CreateElement("products");
             }
 
-            var xRoot = doc.DocumentElement;          
+            var xRoot = doc.DocumentElement;
             foreach (XmlNode xnode in xRoot)
             {
                 if (xnode.Attributes.Count > 0)
@@ -98,11 +99,43 @@ namespace CRMApplications
                     {
                         product.Id = Guid.Parse(attrGuid.Value);
                     }
+                    var changeEntries = new List<Product.ChangeEntry>();
+                    foreach (XmlNode storyNode in xnode.ChildNodes)
+                    {
+                        if (storyNode.Name.Equals("story"))
+                        {
+                            var entry = new Product.ChangeEntry();
+                            entry.ProductName = storyNode.Attributes["productName"].Value;
+                            entry.ProductNumber = Convert.ToInt32(storyNode.Attributes["productNumber"].Value);
+                            entry.Price = Convert.ToDecimal(storyNode.Attributes["price"].Value);
+                            entry.Existence = Convert.ToBoolean(storyNode.Attributes["existence"].Value);
+                            changeEntries.Add(entry);
+                        }
+                    }
+                    product.ChangeEntries = changeEntries;
 
                     products.Add(product);
                 }
             }
             return products;
+        }
+        public static void AddChangeEntriesNode(XmlNode productNode, List<Product.ChangeEntry> entries)
+        {
+            if (entries == null || entries.Count == 0)
+            {
+                return;
+            }
+
+            foreach (var entry in entries)
+            {
+                var changeEntry = productNode.OwnerDocument.CreateElement("story");
+                changeEntry.SetAttribute("productName", entry.ProductName);
+                changeEntry.SetAttribute("productNumber", entry.ProductNumber.ToString());
+                changeEntry.SetAttribute("price", entry.Price.ToString());
+                changeEntry.SetAttribute("existence", entry.Existence.ToString());
+
+                productNode.AppendChild(changeEntry);
+            }
         }
     }
 }
